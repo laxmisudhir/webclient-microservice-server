1ff54b40608959ca4b372eb79fa9ce7d946c84df
laxmisudhir <laxmi.sudhir@gmail.com>
2017-04-26 13:01:50 +0530

    Update WebclientMicroserviceServerApplication.java

--- src/main/java/com/doj/web/WebclientMicroserviceServerApplication.java
+++ src/main/java/com/doj/web/WebclientMicroserviceServerApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 public class WebclientMicroserviceServerApplication {
 	
-	public static final String ACCOUNTS_SERVICE_URL = "http://accounts-microservice-server";
+	public static final String ACCOUNTS_SERVICE_URL = "http://ACCOUNTS-MICROSERVICE";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WebclientMicroserviceServerApplication.class, args);
